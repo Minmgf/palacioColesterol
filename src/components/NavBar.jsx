a39b2fb8a0c4c81e006a541b3e0b6679c50181b1
minmgf <manuelinm2015@gmail.com>
2023-11-01 17:49:34 -0500

    NAVBAR Modificado

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,9 +6,7 @@ const NavBar = () => {
         <div className="menu [grid-area:menu] text-white flex gap-6 text-3xl font-lilitaOne ml-5 pt-4">
         <Link to="/home" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Inicio</Link>
         <Link to="/contact" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Quienes Somos</Link>
-        <a href="#" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Sedes</a>
         <Link to="/product" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Productos</Link>        
-        <a href="#" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>API</a>
         </div>
         <div className="title [grid-area:title] [color:#BF7728] font-lilitaOne text-3xl text-end pr-5 pt-4">
             <a href="#">
