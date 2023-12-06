cdfef8e6f096abeb90fe0b9c65df4d9a41998b66
Manuel Isaias Navarro Montealegre <manuelinm2015@gmail.com>
2023-12-06 17:08:51 -0500

    Add Build

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
     <div id='navBar' className='w-full h-16 [background-color:#1A1B1D]'>
         <div className="menu [grid-area:menu] text-white flex gap-6 text-3xl font-lilitaOne ml-5 pt-4">
         <Link to="/home" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Inicio</Link>
-        <a href="#" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Quienes Somos</a>
+        {/* <a href="#" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Quienes Somos</a> */}
         <Link to="/contact" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Sedes</Link>
         <Link to="/product" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Productos</Link>        
         <Link to="/login" className='hover:text-orange-400 active:scale-[.98] hover:underline underline-offset-4 decoration-gray-800'>Login </Link>        
